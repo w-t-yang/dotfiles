f053a0e960693f39abd46e5fe1032b6766e0e801
Wentao Yang <winterock.yang@gmail.com>
2023-03-02 15:48:06 +0800

    Org capture templates

--- .wy.emacs.d/wy-org-settings.el
+++ .wy.emacs.d/wy-org-settings.el
@@ -10,15 +10,15 @@
 (require-package 'org-bullets)
 (add-hook 'org-mode-hook 'org-bullets-mode)
 (add-hook 'org-mode-hook 'org-indent-mode)
-(setq-default org-bullets-bullet-list '("❄" "❄" "❄" "❄" "☢")
-              org-list-demote-modify-bullet '(("+" . "-") ("-" . "+") ("*" . "+"))
-              org-list-allow-alphabetical t
-              )
+(setq-default
+ ;;org-bullets-bullet-list '("❄" "❄" "❄" "❄" "☢")
+ org-bullets-bullet-list '("⦿" "◉" "●" "○" "・")
+ org-list-demote-modify-bullet '(("+" . "-") ("-" . "+") ("*" . "+"))
+ org-list-allow-alphabetical t
+ )
 
 (defvar org-project-root "~/Projects/orgfiles")
-(defvar org-todos-file (format "%s/%s" org-project-root "todos"))
-(defvar org-notes-file (format "%s/%s" org-project-root "notes"))
-(defvar org-work-file (format "%s/%s" org-project-root "work"))
+(defvar org-capture-file (format "%s/%s.org" org-project-root "captured"))
 
 (defadvice org-capture-finalize
     (after delete-capture-frame activate)
@@ -32,41 +32,35 @@
   (if (equal "org capture" (frame-parameter nil 'name))
       (delete-frame)))
 
-(setq-default org-capture-templates
-              `(
-                ("t" "todo" entry
-                 (file+headline ,org-todos-file "Upcomming Tasks")
-                 "
-* TODO %? :UNCATTED:
-  %U
-  Link: %a"
-                 :clock-resume t)
+(defvar wy-org-capture-categories
+  `(("t" "todo")
+    ("w" "work")
+    ("e" "emacs")
+    ("n" "note")
+    ("i" "idea"))
+  )
 
-                ("e" "emacs todo" entry
-                 (file+headline ,org-todos-file)
-                 "
-* TODO %? :EMACS:
-  %U
-  Link: %a"
-                 :clock-resume t)
+(defvar wy-generated-org-capture-templates ())
 
-                ("w" "work todo" entry
-                 (file+olp+datetree ,org-work-file)
-                 "
-* TODO %? :WORK:
-  %U"
-                 :clock-resume t)
+(defun wy-generate-org-capture-templates ()
+  "Generate org templates."
+  (setq wy-generated-org-capture-templates ())
+  (dolist (item wy-org-capture-categories)
+    (let ((key (car item))
+          (tag (car (cdr item)))
+          )
+      (push `(,key ,tag entry (file+olp+datetree ,org-capture-file)
+                   ,(concat "\n* %? :" tag ":\n%U\nLink: %a")
+                   :clock-resume t)
+            wy-generated-org-capture-templates)
+      )
+    )
+  )
+(wy-generate-org-capture-templates)
 
-                ("n" "note" entry
-                 (file+olp+datetree ,org-notes-file)
-                 "
-* %? :NOTE:
-  %U
-  Link: %a"
-                 :clock-resume t)
-                )
-              )
-(require-package 'noflet)
+(setq-default org-capture-templates wy-generated-org-capture-templates)
+
+;; (require-package 'noflet)
 
 (defun make-capture-frame ()
   "Create a new frame and run 'org-capture'."
@@ -84,7 +78,7 @@
   )
 
 ;; (global-set-key (kbd "M-c") 'org-capture)
-(global-set-key (kbd "C-*") 'make-capture-frame)
+;; (global-set-key (kbd "C-*") 'make-capture-frame)
 
 ;; org-reveal for presentation in Emacs
 (require-package 'ox-reveal)
