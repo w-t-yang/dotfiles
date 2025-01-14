3f68dd8163ee8ff98e6fba5cd8dd7c750ea0ae0a
Wentao Yang <wentao.yang@booking.com>
2025-01-14 17:25:52 +0800

    Update emacs java config

--- .wy.emacs.d/wy-java.el
+++ .wy.emacs.d/wy-java.el
@@ -57,28 +57,91 @@
     (insert (concat "mvn test -Dtest=" marked-test)))
   )
 
-(defun flymake-checkstyle-init (report-fn &rest _args)
-  "Initialize flymake for Checkstyle.
-REPORT-FN is the callback function to report diagnostics."
-  (let* ((checkstyle-jar "~/checkstyle/checkstyle.jar")
-         (checkstyle-config "~/checkstyle/checkstyle.xml")
-         (source (buffer-file-name))
-         (command (list "java" "-jar" checkstyle-jar "-c" checkstyle-config)))
-    (list :command command
-          :error-patterns
-          '(("^[ERROR] \\(.*\\):\\([0-9]+\\):[0-9]*[:]* \\(.*\\)$" 1 2 nil 3))
-          :source source
-          :report-fn report-fn)))
+;; (require-package 'google-c-style)
+;; (autoload 'google-set-c-style "google-c-style")
+;; (autoload 'google-make-newline-indent "google-c-style")
+;; (add-hook 'java-mode-hook 'google-set-c-style)
+;; (add-hook 'java-mode-hook 'google-make-newline-indent)
 
-(defun flymake-checkstyle-load ()
-  "Configure flymake mode to check the current buffer's Java syntax."
-  (interactive)
-  (add-hook 'flymake-diagnostic-functions #'flymake-checkstyle-init nil t)
-  (flymake-mode t))
 
-(setq flymake-proc-allowed-file-name-masks nil)
-(setq flymake-diagnostic-functions (delq 'flymake-proc-legacy-flymake flymake-diagnostic-functions))
-(add-hook 'java-mode-hook 'flymake-checkstyle-load)
-;; (setq flymake-log-level 3)
+;; Set up checkstyle flymake backend
+;; Reference: https://www.gnu.org/software/emacs/manual/html_node/flymake/An-annotated-example-backend.html
+(defvar-local checkstyle--flymake-proc nil)
+(defvar-local checkstyle-jar "~/checkstyle/checkstyle.jar")
+(defvar-local checkstyle-xml "~/checkstyle/google_checks.xml")
+(defvar-local checkstyle-regex "^\\(\\[.*\\]\\) \\(.*.java\\):\\([0-9]+\\):[0-9]*[:]* \\(.*\\)$")
+
+;; (defun checkstyle-flymake (report-fn &rest _args)
+;;   "Initialize flymake for Checkstyle.
+;; REPORT-FN is the callback function to report diagnostics."
+;;   (let* ((source (buffer-file-name))
+;;          (command (list "java" "-jar" checkstyle-jar "-c" checkstyle-xml)))
+;;     (list :command command
+;;           :error-patterns
+;;           ;; '(("^\\[WARN\\] \\(.*\\):\\([0-9]+\\):[0-9]*[:]* \\(.*\\)$" 1 2 nil 3))
+;;           '(("^\\(.*.java\\):\\([0-9]+\\):[0-9]*[:]* \\(.*\\)$" 1 2 nil 3))
+;;           :source source
+;;           :report-fn report-fn)))
+
+(defun flymake-checkstyle-backend (report-fn &rest _args)
+  "Flymake backend for Checkstyle.
+
+Calls REPORT-FN with a list of Flymake diagnostics for the current buffer."
+
+  (unless (executable-find "java")
+    (error "Cannot find java"))
+
+  (when (process-live-p checkstyle--flymake-proc)
+    (kill-process checkstyle--flymake-proc))
+
+  (let* ((source (current-buffer))
+         (filename (buffer-file-name source)))
+    (save-restriction
+      (widen)
+      (setq checkstyle--flymake-proc
+            (make-process
+             :name "flymake-checkstyle" :noquery t :connection-type 'pipe
+             :buffer (generate-new-buffer " *flymake-checkstyle*")
+             :command (list "java" "-jar" checkstyle-jar "-c" checkstyle-xml (buffer-file-name))
+             :sentinel
+             (lambda (proc _event)
+               (when (memq (process-status proc) '(exit signal))
+                 (unwind-protect
+                     (if (with-current-buffer (current-buffer) (eq proc checkstyle--flymake-proc))
+                         (with-current-buffer (process-buffer proc)
+                           (goto-char (point-min))
+                           (cl-loop
+                            while (search-forward-regexp
+                                   checkstyle-regex
+                                   nil t)
+                            for msg = (match-string 4)
+                            for (beg . end) = (flymake-diag-region
+                                               (current-buffer)
+                                               (string-to-number (match-string 3)))
+                            for type = (if (string-match "WARN" (match-string 1))
+                                           :warning
+                                         :error)
+                            when (and beg end)
+                            collect (flymake-make-diagnostic source beg end type msg)
+                            into diags
+                            finally (funcall report-fn diags)))
+                       (flymake-log :warning "Canceling obsolete check %s" proc))
+                   ;;(kill-buffer (process-buffer proc))
+                   )))))
+      (process-send-region checkstyle--flymake-proc (point-min) (point-max))
+      (process-send-eof checkstyle--flymake-proc))))
+
+;; (defun flymake-checkstyle-load ()
+;;   "Configure flymake mode to check the current buffer with checkstyle."
+;;   (add-hook 'flymake-diagnostic-functions #'flymake-checkstyle-backend nil t))
+;; (add-hook 'java-mode-hook 'flymake-checkstyle-load)
+
+;; (require-package 'flycheck)
+;; (flycheck-define-checker checkstyle-checker
+;;   "Java checkstyle checker."
+;;   :command ("java" "-jar" "~/checkstyle/checkstyle.jar" "-c" "~/checkstyle/google_checks.xml" "-f" "xml" source)
+;;   :error-parser flycheck-parse-checkstyle
+;;   :enable t
+;;   :modes (java-mode))
 
 ;;; wy-java.el ends here
